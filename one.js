b1078c573cdf8d55d57344c0affdf645086afbe7
ManojDalavoyD <dalavoymanoj@gmail.com>
2025-04-23 15:54:56 +0530

    variables

--- one.js
+++ one.js
@@ -1,1 +1,27 @@
-console.log("hi Friends ");
+//variables container to store data values 
+//integer/number data type 
+var num1=44;
+var num2=55;
+//string data type
+var str1="hello";
+var str2="world";
+//boolean data type
+var bool1=true;
+var bool2=false;
+console.log(bool1+bool2); // true
+//object data type
+var marks={
+    math: 90,
+    english: 80,
+    science: 85
+}
+console.log(marks)
+//undefined data type
+var x;
+console.log(x); 
+//null data type
+var y=null;
+console.log(num1+num2); // 99
+//array data type
+var arr=[1,2,3,4,5];
+console.log(arr); // 1

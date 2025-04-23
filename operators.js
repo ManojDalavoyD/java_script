e94d8feff3e09d97c076585191c4f3d45c5aa6f7
ManojDalavoyD <dalavoymanoj@gmail.com>
2025-04-23 15:58:56 +0530

    arthimetic operations

---
+++ operators.js
@@ -0,0 +1,35 @@
+// Arithmetic Operators in JavaScript
+
+// Addition
+let sum = 10 + 5; // 15
+console.log("Addition:", sum);
+
+// Subtraction
+let difference = 10 - 5; // 5
+console.log("Subtraction:", difference);
+
+// Multiplication
+let product = 10 * 5; // 50
+console.log("Multiplication:", product);
+
+// Division
+let quotient = 10 / 5; // 2
+console.log("Division:", quotient);
+
+// Modulus (Remainder)
+let remainder = 10 % 3; // 1
+console.log("Modulus:", remainder);
+
+// Exponentiation
+let power = 2 ** 3; // 8
+console.log("Exponentiation:", power);
+
+// Increment
+let increment = 5;
+increment++; // 6
+console.log("Increment:", increment);
+
+// Decrement
+let decrement = 5;
+decrement--; // 4
+console.log("Decrement:", decrement);
